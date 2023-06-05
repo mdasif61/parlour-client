d303e38c11dabd2105303ae35a672fa0e314b11c
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-05 22:00:01 +0600

    payment page start

--- src/Dashboard/BookingList.jsx
+++ src/Dashboard/BookingList.jsx
@@ -2,19 +2,21 @@ import useBookings from "../hooks/useBookings";
 import ListBooked from "./ListBooked";
 
 const BookingList = () => {
+  const { bookedlist } = useBookings();
 
-    const {bookedlist}=useBookings()
-
-    return (
-        <div className="grid md:grid-cols-2 grid-cols-1 gap-7 p-20">
-            {
-                bookedlist.map(list=><ListBooked
-                key={list._id}
-                list={list}
-                ></ListBooked>)
-            }
-        </div>
-    );
+  return bookedlist.length > 0 ? (
+    <>
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-7 p-20">
+        {bookedlist.map((list) => (
+          <ListBooked key={list._id} list={list}></ListBooked>
+        ))}
+      </div>
+    </>
+  ) : (
+    <div className="min-h-screen w-full flex items-center justify-center">
+        <h1 className="text-gray-500 font-bold text-3xl">Not Found Booked Service</h1>
+    </div>
+  );
 };
 
 export default BookingList;
--- src/hooks/useServices.jsx
+++ src/hooks/useServices.jsx
@@ -1,18 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 const useServices = () => {
   const [serviceLimit, setServiceLimit] = useState(3);
   const [services,setServices]=useState([])
-//   const { data: services = [], refetch } = useQuery({
-//     queryKey: ["services"],
-//     queryFn: async () => {
-//       const res = await fetch(
-//         `http://localhost:5000/services?limit=${serviceLimit}`
-//       );
-//       return res.json();
-//     },
-//   });
 
     useEffect(()=>{
         fetch(`http://localhost:5000/services?limit=${serviceLimit}`)
